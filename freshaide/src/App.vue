29df9faffae51c2fccb4b7f194ba995da12e67f3
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-08-05 23:13:09 -0400

    dock position

--- freshaide/src/App.vue
+++ freshaide/src/App.vue
@@ -1,6 +1,26 @@
 <template>
-  <div id="app">123</div>
+  <div class="dock">
+    <span class="docker__item">Home</span>
+    <span class="docker__item">Cart</span>
+    <span class="docker__item">Order</span>
+    <span class="docker__item">Profile</span>
+  </div>
 </template>
 
 <style lang="scss">
+.dock {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+}
+.docker__item {
+  flex: 1;
+  text-align: center;
+}
 </style>
