bea904aa07f00eed5bc1bcb460f6119b81cb8362
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-08-09 20:10:08 -0400

    finished search bar prototype

--- freshaide/src/App.vue
+++ freshaide/src/App.vue
@@ -3,26 +3,31 @@
     <div class="wrapper">
       <!-- address bar -->
       <div class="address">
-        <span class="iconfont address__icon">&#xe651;</span>
+        <span class="iconfont address__icon">&#xe758;</span>
         400 Eastcastle Place, London, ON
-        <span class="iconfont address__notice">&#xe70a;</span>
+        <span class="iconfont address__notice">&#xe75a;</span>
+      </div>
+      <!-- search bar -->
+      <div class="search">
+        <span class="iconfont">&#xe7b4;</span>
+        <span class="seach__label">What are you looking for?</span>
       </div>
     </div>
     <div class="dock">
       <span class="dock__nav_elem dock__nav_elem--active"
-        ><div class="iconfont">&#xe6bb;</div>
+        ><div class="iconfont">&#xe7a7;</div>
         <div class="dock__label">Home</div>
       </span>
       <span class="dock__nav_elem"
-        ><div class="iconfont">&#xe6b9;</div>
+        ><div class="iconfont">&#xe747;</div>
         <div class="dock__label">Cart</div>
       </span>
       <span class="dock__nav_elem"
-        ><div class="iconfont">&#xe7ab;</div>
+        ><div class="iconfont">&#xe7b3;</div>
         <div class="dock__label">Orders</div>
       </span>
       <span class="dock__nav_elem">
-        <div class="iconfont">&#xe77a;</div>
+        <div class="iconfont">&#xe7c9;</div>
         <div class="dock__label">Profile</div>
       </span>
     </div>
@@ -31,8 +36,8 @@
 
 <style lang="scss">
 //import predefined scss files
-@import './style/variables.scss';
-@import './style/mixins.scss';
+@import "./style/variables.scss";
+@import "./style/mixins.scss";
 
 .wrapper {
   position: absolute;
@@ -48,6 +53,8 @@
   padding: 0.16rem 0.24rem 0.16rem 0;
   line-height: 0.22rem;
   font-size: 0.16rem;
+  color: $content-font-color;
+  font-family: $content-font;
   @include ellipsis;
   .address__icon {
     position: relative;
@@ -60,7 +67,27 @@
     top: 0.17rem;
     font-size: 0.2rem;
   }
-  color: $content-font-color;
+}
+
+.search {
+  margin-bottom: 0.12rem;
+  vertical-align: middle;
+  line-height: 0.32rem;
+  background: #f7f7ff;
+  color: #577399;
+  border-radius: 0.16rem;
+  font-family: $content-font;
+  .iconfont {
+    position: relative;
+    top: 0.02rem;
+    display: inline-block;
+    padding: 0 0.05rem 0 0.1rem;
+    font-size: 0.16rem;
+  }
+  &__label{
+    display: inline-block;
+    font-size: 0.14rem;
+  }
 }
 
 .dock {
@@ -75,6 +102,7 @@
   height: 0.49rem;
   border-top: 0.01 solid #f1f1f1;
   color: $content-font-color;
+  font-family: $content-font;
   //navigation element
   &__nav_elem {
     flex: 1;
