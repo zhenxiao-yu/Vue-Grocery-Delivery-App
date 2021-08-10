e7a3cf4040c488aed148bc3ce3346ef5c8c551bf
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-08-10 00:15:41 -0400

    nearby stores basic template

--- freshaide/src/App.vue
+++ freshaide/src/App.vue
@@ -20,47 +20,63 @@
       <div class="cate">
         <!-- icon 1 -->
         <div class="cate__item">
-          <img class="cate__item__icon" src="./images/Snacks.png"/>
+          <img class="cate__item__icon" src="./images/Snacks.png" />
           <p class="cate__item__label">Snacks</p>
         </div>
         <!-- icon 2 -->
         <div class="cate__item">
-          <img class="cate__item__icon" src="./images/Pantry.png"/>
+          <img class="cate__item__icon" src="./images/Pantry.png" />
           <p class="cate__item__label">Pantry</p>
         </div>
         <!-- icon 3 -->
         <div class="cate__item">
-          <img class="cate__item__icon" src="./images/Meat.png"/>
+          <img class="cate__item__icon" src="./images/Meat.png" />
           <p class="cate__item__label">Meat</p>
         </div>
         <!-- icon 4 -->
         <div class="cate__item">
-          <img class="cate__item__icon" src="./images/Study.png"/>
+          <img class="cate__item__icon" src="./images/Study.png" />
           <p class="cate__item__label">School Supplies</p>
         </div>
         <!-- icon 5 -->
         <div class="cate__item">
-          <img class="cate__item__icon" src="./images/Health.png"/>
+          <img class="cate__item__icon" src="./images/Health.png" />
           <p class="cate__item__label">Health</p>
         </div>
         <!-- icon 6 -->
         <div class="cate__item">
-          <img class="cate__item__icon" src="./images/Fruit.png"/>
+          <img class="cate__item__icon" src="./images/Fruit.png" />
           <p class="cate__item__label">Fruits</p>
         </div>
         <!-- icon 7 -->
         <div class="cate__item">
-          <img class="cate__item__icon" src="./images/Beverage.png"/>
+          <img class="cate__item__icon" src="./images/Beverage.png" />
           <p class="cate__item__label">Drinks</p>
         </div>
         <!-- icon 8 -->
         <div class="cate__item">
-          <img class="cate__item__icon" src="./images/Vegetable.png"/>
+          <img class="cate__item__icon" src="./images/Vegetable.png" />
           <p class="cate__item__label">Vegetables</p>
         </div>
       </div>
       <!-- divider -->
-      <div class="divide">
+      <div class="divide"></div>
+      <!-- near by stores -->
+      <div class="closeby">
+        <h3 class="nearby__title">Stores Near Me</h3>
+        <!-- store 1 -->
+        <div class="closeby__item">
+          <img class="closeby__item__image" src="./images/Walmart.png" />
+          <div class="closeby__content">
+            <div class="closeby_content_title">Walmart</div>
+            <div class="closeby_content__tags">
+              <div class="closeby_content__tag">10000+ Items Sold This Month</div>
+              <div class="closeby_content__tag">$3.99 Delivery Fee</div>
+              <div class="closeby_content__tag">92% Satisfaction Rate</div>
+            </div>
+            <p class="closeby_content__vip">70% Off Delivery Fee For VIP Members</p>
+          </div>
+        </div>
       </div>
     </div>
     <div class="dock">
@@ -134,48 +150,49 @@
     padding: 0 0.05rem 0 0.1rem;
     font-size: 0.16rem;
   }
-  &__label{
+  &__label {
     display: inline-block;
     font-size: 0.14rem;
   }
 }
 
-.banner{
+.banner {
   height: 0;
   overflow: hidden;
   padding-bottom: 33.3%;
-  &__image{
+  &__image {
     width: 100%;
     border-radius: 0.05rem;
   }
 }
 
-.cate{
+.cate {
   display: flex;
   flex-wrap: wrap;
-  margin-top: .16rem;
-  &__item{
+  margin-top: 0.16rem;
+  &__item {
     width: 25%;
-    &__icon{
+    &__icon {
       display: block;
       width: 0.5rem;
-      height:0.5rem;
+      height: 0.5rem;
       //center
       margin: 0 auto;
     }
-    &__label{
+    &__label {
       text-align: center;
       margin: 0.06rem 0 0.16rem 0;
       font-family: $content-font;
-      color:$content-font-color;
+      color: $content-font-color;
     }
   }
 }
 
-.divide{
-  margin: 0 -.18rem;
-  height: .1rem;
+.divide {
+  margin: 0 -0.18rem;
+  height: 0.1rem;
   background: $content-bg-color;
+  border-radius: 0 0 0.075rem 0.075rem;
 }
 
 .dock {
