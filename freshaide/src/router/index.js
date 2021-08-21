80cedc15c72c82afe7f2e8fbefe4617d73014d92
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-08-20 21:59:39 -0400

    code clean up

--- freshaide/src/router/index.js
+++ freshaide/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/HomePage/Home'
 import Login from '../views/LoginPage/Login'
+import Register from '../views/RegisterPage/Register'
 
 const routes = [
   {
@@ -13,29 +14,17 @@ const routes = [
     component: Login,
     // execute before entering the page
     beforeEnter (to, from, next) {
-      const loggedIn = localStorage.loggedIn
-      if (loggedIn) {
-        next({ name: 'Home' })
-      } else {
-        next()
-      }
-      console.log(to, from)
-      next()
+      const { loggedIn } = localStorage.loggedIn
+      loggedIn ? next({ name: 'Home' }) : next()
     }
   }, {
     path: '/register',
-    name: 'register',
-    component: Login,
+    name: 'Register',
+    component: Register,
     // execute before entering the page
     beforeEnter (to, from, next) {
-      const loggedIn = localStorage.loggedIn
-      if (loggedIn) {
-        next({ name: 'Home' })
-      } else {
-        next()
-      }
-      console.log(to, from)
-      next()
+      const { loggedIn } = localStorage.loggedIn
+      loggedIn ? next({ name: 'Home' }) : next()
     }
   }
 ]
@@ -47,14 +36,10 @@ const router = createRouter({
 
 // execute each time whe router switch to new page
 router.beforeEach((to, from, next) => {
-  const loggedIn = localStorage.loggedIn
-  if (loggedIn || to.name === 'Login') {
-    next()
-  } else {
-    next({ name: 'Login' })
-  }
-  console.log(to, from)
-  next()
+  const { loggedIn } = localStorage
+  const { name } = to
+  const isLoginOrRegister = (name === 'Login' || name === 'Register');
+  (loggedIn || isLoginOrRegister) ? next() : next({ name: 'Login' })
 })
 
 export default router
