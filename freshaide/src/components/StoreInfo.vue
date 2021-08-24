fce82e8414215fbbab116e56a669dc5fa87099a6
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-08-24 17:30:46 -0400

    Component extraction

---
+++ freshaide/src/components/StoreInfo.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="store">
+      <img :src="item.img" class="store__image">
+      <div class="store__content">
+        <div class="store__content__title">{{item.name}}</div>
+        <div class="store__content__tags">
+          <span class="store__content__tag">Sales: {{item.sales}}+ </span>
+          <span class="store__content__tag">${{item.deliveryFee}} delivery fee</span>
+        </div>
+        <div class="store__content__tags">
+          <span class="store__content__tag">{{item.distance}} km away </span>
+          <span class="store__content__tag">Google Rating: {{item.rating}} Stars</span>
+        </div>
+        <p class="store__content__vip">{{item.vip}}</p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'StoreInfo',
+  props: ['item']
+}
+</script>
+
+<style lang="scss" scoped>
+//import predefined scss files
+@import "../style/variables";
+
+.store {
+    display: flex;
+    padding: 0.12rem;
+    &__image {
+      margin: 0.16rem 0.20rem 0 0;
+      top: 50%;
+      height: 0.56rem;
+      width: 0.56rem;
+      border-radius: 50%;
+      box-shadow: 0 0 0.015rem 0.015rem $content-highlight-color;
+    }
+    &__content {
+    flex: 1;
+    padding-bottom: 0.12rem;
+    border-bottom: 1px solid $content-border-color;
+    &__title {
+      line-height: 0.22rem;
+      font-size: 0.16rem;
+      font-family: $content-font;
+      color: $content-font-color;
+    }
+    &__tags {
+      margin-top: 0.08rem;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      font-family: $content-font;
+      color: $content-font-color;
+    }
+    &__tag {
+      margin-right: 0.16rem;
+    }
+    &__vip {
+      margin: 0.08rem 0 0 0;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: $content-highlight-color;
+      font-family: $content-font;
+    }
+  }
+}
+</style>
--- freshaide/src/views/HomePage/Closeby.vue
+++ freshaide/src/views/HomePage/Closeby.vue
@@ -2,32 +2,14 @@
   <!-- near by stores -->
   <div class="closeby">
     <h3 class="closeby__title">Stores Near Me</h3>
-    <!-- store 1 -->
-    <div
-      class="closeby__item"
-      v-for="item in closebyList"
-      :key="item.id"
-    >
-      <img :src="item.img" class="closeby__item__image">
-      <div class="closeby__content">
-        <div class="closeby__content__title">{{item.name}}</div>
-        <div class="closeby__content__tags">
-          <span class="closeby__content__tag">Sales: {{item.sales}}+ </span>
-          <span class="closeby__content__tag">${{item.deliveryFee}} delivery fee</span>
-        </div>
-        <div class="closeby__content__tags">
-          <span class="closeby__content__tag">{{item.distance}} km away </span>
-          <span class="closeby__content__tag">Google Rating: {{item.rating}} Stars</span>
-        </div>
-        <p class="closeby__content__vip">{{item.vip}}</p>
-      </div>
-    </div>
+    <StoreInfo v-for="item in closebyList" :key="item.id" :item="item"/>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 import { get } from '../../utils/req'
+import StoreInfo from '../../components/StoreInfo'
 
 const useClosebyListEffect = () => {
   const closebyList = ref([])
@@ -42,6 +24,7 @@ const useClosebyListEffect = () => {
 
 export default {
   name: 'Closeby',
+  components: { StoreInfo },
   setup () {
     const { closebyList, getClosebyList } = useClosebyListEffect()
     getClosebyList()
@@ -54,7 +37,6 @@ export default {
 <style lang="scss" scoped>
 //import predefined scss files
 @import "../../style/variables.scss";
-
 .closeby {
   &__title {
     margin: 0.16rem 0 0.02rem 0;
@@ -62,46 +44,6 @@ export default {
     font-weight: nromal;
     font-family: $content-font;
     color: $content-font-color;
-  }
-  &__item {
-    display: flex;
-    padding: 0.12rem;
-    &__image {
-      margin: 0.16rem 0.20rem 0 0;
-      top: 50%;
-      height: 0.56rem;
-      width: 0.56rem;
-      border-radius: 50%;
-      box-shadow: 0 0 0.015rem 0.015rem $content-highlight-color;
-    }
-  }
-  &__content {
-    flex: 1;
-    padding-bottom: 0.12rem;
-    border-bottom: 1px solid $content-border-color;
-    &__title {
-      line-height: 0.22rem;
-      font-size: 0.16rem;
-      font-family: $content-font;
-      color: $content-font-color;
-    }
-    &__tags {
-      margin-top: 0.08rem;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      font-family: $content-font;
-      color: $content-font-color;
-    }
-    &__tag {
-      margin-right: 0.16rem;
-    }
-    &__vip {
-      margin: 0.08rem 0 0 0;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      color: $content-highlight-color;
-      font-family: $content-font;
-    }
   }
 }
 </style>
