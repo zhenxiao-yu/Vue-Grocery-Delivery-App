584ec90b643e4a73dba741ebc59ad963153fb87c
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-09-14 12:46:13 -0400

    order confirmation page division

---
+++ freshaide/src/views/ConfirmPage/Order.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="order">
+        <div class="order__total-price">Order Total: <b> &#36;{{myCalculator.price}} </b> </div>
+        <div class="order__btn">Confirm</div>
+    </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+import { useCommonCartEffect } from '../../effects/cartEffects'
+
+export default {
+  name: 'Order',
+  setup () {
+    const route = useRoute()
+    const storeId = route.params.id
+    const { myCalculator } = useCommonCartEffect(storeId)
+    return { myCalculator }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+//import predefined scss files
+@import "../../style/variables.scss";
+.order {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  height: 0.49 rem;
+  line-height: 0.49rem;
+  background: #fff;
+  &__total-price {
+    flex: 1;
+    text-indent: 0.24rem;
+    font-size: 0.14rem;
+    color: $content-font-color;
+  }
+  &__btn {
+    width: 0.98rem;
+    background-color: $content-highlight-color;
+    color: $content-bg-color;
+    text-align: center;
+    font-size: 0.14rem;
+  }
+}
+</style>
--- freshaide/src/views/ConfirmPage/OrderConfirmation.vue
+++ freshaide/src/views/ConfirmPage/OrderConfirmation.vue
@@ -2,28 +2,18 @@
   <div class="wrapper">
     <HeaderArea />
     <OrderList />
-    <div class="order">
-        <div class="order__total-price">Order Total: <b> &#36;{{myCalculator.price}} </b> </div>
-        <div class="order__btn">Confirm</div>
-    </div>
+    <Order />
   </div>
 </template>
 
 <script>
-import { useRoute } from 'vue-router'
-import { useCommonCartEffect } from '../../effects/cartEffects'
 import HeaderArea from './HeaderArea'
 import OrderList from './OrderList'
+import Order from './Order'
 
 export default {
   name: 'OrderConfirmation',
-  components: { HeaderArea, OrderList },
-  setup () {
-    const route = useRoute()
-    const storeId = route.params.id
-    const { storeName, productList, myCalculator } = useCommonCartEffect(storeId)
-    return { storeName, productList, myCalculator }
-  }
+  components: { HeaderArea, OrderList, Order }
 }
 </script>
 
@@ -38,33 +28,9 @@ export default {
   top: 0;
   bottom: 0;
   background-color: #eee;
-   overflow-y:scroll;
+  overflow-y:scroll;
   color: $content-font-color;
   font-family: $content-font;
-}
-
-.order {
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  height: 0.49 rem;
-  line-height: 0.49rem;
-  background: #fff;
-  &__total-price {
-    flex: 1;
-    text-indent: 0.24rem;
-    font-size: 0.14rem;
-    color: $content-font-color;
-  }
-  &__btn {
-    width: 0.98rem;
-    background-color: $content-highlight-color;
-    color: $content-bg-color;
-    text-align: center;
-    font-size: 0.14rem;
-  }
 }
 
 </style>
